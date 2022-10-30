d5da88d71b027f689af1975d4220d49bc59b443e
Jay Bazuzi <jay@bazuzi.com>
2022-10-30 15:14:25 -0700

    t - add test_example_usage

---
+++ tests/test_example_usage.py
@@ -0,0 +1,22 @@
+import re
+import approvaltests
+import approvaltests.approvals
+import contextlib
+import io
+import unittest
+import unittest.mock
+
+import example_usage
+import human_do_task
+
+def test__verify_happy_path() -> None:
+    with contextlib.redirect_stdout(io.StringIO()) as stdout:
+        with unittest.mock.patch("builtins.input", side_effect=["V", "", "Y"]):
+            human_do_task.Process.run(example_usage.perform, example_usage.verify)
+
+    approvaltests.approvals.verify(
+        stdout.getvalue(),
+        options=approvaltests.Options().with_scrubber(
+            lambda s: re.sub(r"\d+:\d+:\d+(\.\d+)?", "{REMOVED}", s)
+        ),
+    )
--- tests/test_samples.py
+++
@@ -1,5 +0,0 @@
-import approvaltests.approvals
-
-
-def test_pytest() -> None:
-    approvaltests.approvals.verify(42)
