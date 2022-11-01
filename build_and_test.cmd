d987b324d44aa61b78e7b7cc0b0bc868bdd26549
Jay Bazuzi <jay@bazuzi.com>
2022-11-01 15:07:40 -0700

    e - run with currently-installed Python

--- build_and_test.cmd
+++ build_and_test.cmd
@@ -1,1 +1,2 @@
-@python -m pip install tox --quiet --constraint constraints.txt  --disable-pip-version-check && tox --quiet --parallel
+@REM `-e py` is a predefined environment factor. See https://tox.wiki/en/latest/config.html#tox-environments
+@python -m pip install tox --quiet --constraint constraints.txt  --disable-pip-version-check && tox -e py,check_formatting --quiet --parallel
