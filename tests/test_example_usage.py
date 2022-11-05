87c290c8c4308cf3ec9375d1a7d0ad12bc79d2ca
Jay Bazuzi <jay@bazuzi.com>
2022-11-05 10:22:32 -0700

    t - add test for perfom mode

--- tests/test_example_usage.py
+++ tests/test_example_usage.py
@@ -14,7 +14,18 @@ def _scrub_timestamp(s: str) -> str:
     return re.sub(r"\d+:\d+:\d+(\.\d+)?", "{REMOVED}", s)
 
 
-def test__verify_happy_path() -> None:
+def test__verify_happy_path__run() -> None:
+    with contextlib.redirect_stdout(io.StringIO()) as stdout:
+        with unittest.mock.patch("builtins.input", side_effect=["P", "", "", "N", ""]):
+            human_do_task.Process.run(example_usage.perform, example_usage.verify)
+
+    approvaltests.approvals.verify(
+        stdout.getvalue(),
+        options=approvaltests.Options().with_scrubber(_scrub_timestamp),
+    )
+
+
+def test__verify_happy_path__verify() -> None:
     with contextlib.redirect_stdout(io.StringIO()) as stdout:
         with unittest.mock.patch("builtins.input", side_effect=["V", "", "Y"]):
             human_do_task.Process.run(example_usage.perform, example_usage.verify)
