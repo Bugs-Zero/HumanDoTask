5c2ecff4e9d03e5b476c52be72e4fe5ea489a071
Jay Bazuzi <jay@bazuzi.com>
2022-11-05 10:28:29 -0700

    e - fix up machine-setup.ps1

--- machine-setup.ps1
+++ machine-setup.ps1
@@ -1,17 +1,13 @@
 # To run this script directly, run this in an elevated admin PowerShell prompt:
-#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/approvals/ApprovalTests.Python.StarterProject/master/install.windows.ps1 | Invoke-Expression
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/Bugs-Zero/HumanDoTask/master/machine-setup.ps1 | Invoke-Expression
 
 # This script is intended to setup a dev machine from scratch. Very useful for setting up a EC2 instance for mobbing.
 #
 
 
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
-iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/python-pycharm.ps1 | iex
+Invoke-WebRequest -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | Invoke-Expression
+Invoke-WebRequest -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/python-pycharm.ps1 | Invoke-Expression
 
 
-# Clone repo
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.Python.StarterProject.git C:\Code\ApprovalTests.Python.StarterProject
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/Bugs-Zero/HumanDoTask.git C:\Code\HumanDoTask
 
-# Done
-cls
-echo "Done!"
