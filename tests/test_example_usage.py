a1b55e6423e43d80e9cfde526141a8da8d3ae51f
Jay Bazuzi <jay@bazuzi.com>
2022-11-05 14:05:31 -0700

    t - capture `input()` prompts
    by replacing a mock with _RedirectStream

--- tests/test_example_usage.py
+++ tests/test_example_usage.py
@@ -1,4 +1,6 @@
 import re
+import textwrap
+import typing
 import approvaltests
 import approvaltests.approvals
 import contextlib
@@ -14,9 +16,26 @@ def _scrub_timestamp(s: str) -> str:
     return re.sub(r"\d+:\d+:\d+(\.\d+)?", "{REMOVED}", s)
 
 
+class redirect_stdin(contextlib._RedirectStream[typing.IO[str]]):
+    _stream = "stdin"
+
+
 def test__verify_happy_path__run() -> None:
     with contextlib.redirect_stdout(io.StringIO()) as stdout:
-        with unittest.mock.patch("builtins.input", side_effect=["P", "", "", "N", ""]):
+        with redirect_stdin(
+            io.StringIO(
+                textwrap.dedent(
+                    """\
+                    P
+                    
+                    
+                    N
+                    
+                    
+                    """
+                )
+            )
+        ):
             human_do_task.Process.run(example_usage.perform, example_usage.verify)
 
     approvaltests.approvals.verify(
